33f3969e57e9a1b60173890ba9502b4e26d02027
Marek Zelinka <mzelinka17@gmail.com>
2026-01-04 19:22:33 +0100

    setup db schema

---
+++ app/db/schema.py
@@ -0,0 +1,26 @@
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel
+
+
+class UserBase(SQLModel):
+    username: str = Field(unique=True, index=True)
+    email: EmailStr = Field(unique=True, index=True)
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    hashed_password: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserPublic(UserBase):
+    id: int
+
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
