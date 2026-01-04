b55f611e09566d39686eefa0e5c2096418275d5d
Marek Zelinka <mzelinka17@gmail.com>
2026-01-04 19:07:08 +0100

    setup

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from .db.session import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from fastapi-jwt-auth!")
-
-
-if __name__ == "__main__":
-    main()
